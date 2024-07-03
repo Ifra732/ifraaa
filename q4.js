4962255e7e533e54af34f12f190a1448e862c984
Ifra732 <ifrariaz732@gmail.com>
2024-07-03 18:56:26 +0500

    Add files via upload

---
+++ q4.js
@@ -0,0 +1,5 @@
+var famousperson = "Albert Einstime";
+var famousquote = "all kids are different flowers of garden ";
+//compose message
+var message = "$(famousperson)once said $(famousquote);";
+console.log(message);
